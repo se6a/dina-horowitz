500ac61bedc61e2fcea2e2bf429f68c20ae9d195
se6a <wyss.sebastian@gmail.com>
2023-01-13 19:57:40 +0100

    add impressum

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -11,9 +11,57 @@
   <div class="space-s" />
 
   <div class="useColumns">
-    <div class="box">Kommt Demnächst.</div>
+    <div class="box">
+      <ul>
+        <li>
+          <address>
+            <span>Dina Horowitz</span>
+            <span>Unterer Batterieweg 85</span>
+            <span>4059 Basel, Schweiz</span>
+          </address>
+          <a class="textLink" href="mailto:info@dinahorowitz.ch" target="_blank" rel="noreferrer">
+            info@dinahorowitz.ch
+          </a>
+        </li>
+
+        <li>
+          <a class="textLink" href="https://www.dinahorowitz.ch" target="_blank" rel="noreferrer">
+            Text, Redaktion & Angebot: Dina Horowitz
+          </a>
+        </li>
+        <li>
+          <a class="textLink" href="https://gianbesset.ch" target="_blank" rel="noreferrer">
+            Design: Gian Besset Brand Design
+          </a>
+        </li>
+        <li>
+          <a class="textLink" href="https://www.sarahweishaupt.ch" target="_blank" rel="noreferrer">
+            Illustrationen: Sarah Weishaupt
+          </a>
+        </li>
+        <li>Kreis-Diagramm nach einer Idee von Andreas Hamann</li>
+        <li>
+          <a class="textLink" href="https://www.barbarakern.com" target="_blank" rel="noreferrer">
+            Fotografie: Barbara Kern
+          </a>
+        </li>
+        <li>
+          <a class="textLink" href="https://sebaaa.ch/" target="_blank" rel="noreferrer">
+            Programmierung & Animation: Sebastian Wyss
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </Page>
 
 <style>
+  address {
+    display: flex;
+    flex-direction: column;
+  }
+
+  li + li {
+    margin-top: 1em;
+  }
 </style>
