f55998225fb2e320591aee8cf65f741b0cf06818
se6a <wyss.sebastian@gmail.com>
2023-03-11 19:22:30 +0100

    zIndex bugfix 40

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,13 +34,11 @@
     }
 
     main {
-        /* z-index: 10; */
         transform: translateZ(0);
     }
 
     .test {
-        /* z-index: 100; */
-        transform: translateZ(0);
+        /* transform: translateZ(0); */
     }
 
     .test {
